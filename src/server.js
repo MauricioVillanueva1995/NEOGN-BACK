82c745e501943f9cc7e908d563293fcd5c27a119
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-04 23:10:33 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -7,17 +7,38 @@ const mercadopago = require("mercadopago");
 
 const server = express();
 
+server.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://neogn-front.onrender.com/",
+    "https://neogn-front.onrender.com/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 mercadopago.configure({
   access_token: process.env.MP_TOKEN,
 });
 
-server.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "https://http2.mlstatic.com"],
-  },
-}));
+server.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://http2.mlstatic.com"],
+    },
+  })
+);
 server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"));
 server.use(express.json());
