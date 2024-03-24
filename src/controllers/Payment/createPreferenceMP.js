8d4cd34890b08e2ae9ef656ecf5244f787d88cb6
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-24 20:28:14 -0300

    fix

--- src/controllers/Payment/createPreferenceMP.js
+++ src/controllers/Payment/createPreferenceMP.js
@@ -7,18 +7,18 @@ mercadopago.configure({
 });
 
 const createPreference = async (req, res) => {
-  
+
   try {
     const { items, transaction_amount, userId } = req.body;
     console.log("USER:", userId);
     console.log("ITEMS", items);
     let preference = {
-      transaction_amount,
+      total_amount: transaction_amount,
       items,
       back_urls: {
         success: `${URL_PRODUCTION}`,
         failure: `${URL_PRODUCTION}`,
-        pendnig: `${URL_PRODUCTION}`,
+        pending: `${URL_PRODUCTION}`,
       },
       auto_return: "approved",
       notification_url: `${DB_URL}/api/payment/webhook/${userId}`,
