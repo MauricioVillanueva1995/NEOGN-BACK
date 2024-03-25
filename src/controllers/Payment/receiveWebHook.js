784553f050c66b807d7eaf21c4f959ce833a237e
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-03-24 21:48:02 -0300

    fixes

--- src/controllers/Payment/receiveWebHook.js
+++ src/controllers/Payment/receiveWebHook.js
@@ -1,32 +1,100 @@
+const mercadopago = require("mercadopago");
 const axios = require("axios");
 
-const accessToken = process.env.MP_TOKEN
+const { DB_URL } = process.env;
+
+const processedRequests = new Set();
 
 const receiveWebHook = async (req, res) => {
-  const paymentId = req.query.id;
-
   try {
-    const response = await axios.get(
-      `https://api.mercadopago.com/v1/payments/${paymentId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    const { query } = req;
+    const { params } = req;
+    console.log("params", params.userId);
+    console.log("req.body", req.body);
 
-    if (response.status === 200) {
-      const data = response.data;
-      console.log(data);
+    const topic = query.topic || query.type;
+
+    console.log({ topic });
+
+    const requestId = query.id || query["data.id"];
+
+    if (processedRequests.has(requestId)) {
+      console.log(
+        `Solicitud duplicada recibida con ID: ${requestId}. Ignorando.`
+      );
+      res.send();
+      return;
     }
-    if (response.status === 404) {
-      const data = response.data;
-      console.log(data);
+
+    processedRequests.add(requestId);
+
+    let payment;
+    let body;
+
+    switch (topic) {
+      case "payment":
+        const paymentId = requestId;
+        console.log(topic, "getting payment", paymentId);
+
+        payment = await mercadopago.payment.findById(paymentId);
+
+        console.log("payment.body", payment.body);
+        console.log("payment status", payment.body.status);
+
+        body = await mercadopago.merchant_orders.findById(
+          payment.body.order.id
+        );
+        break;
+
+      case "merchant_order":
+        const orderId = requestId;
+        console.log(topic, "getting merchant order", orderId);
+        body = await mercadopago.merchant_orders.findById(orderId);
+        console.log("showing body", body);
+        break;
+
+      default:
+        console.log("El tema recibido no es válido.");
+        res.send();
+        return;
     }
-    res.sendStatus(200);
+
+    console.log("payment status outside the block", payment?.body?.status);
+    console.log("body merchant order", body);
+
+    var paidAmount = 0;
+
+    if (payment?.body?.status === "approved") {
+      paidAmount += payment.body.transaction_amount;
+    }
+
+    if (paidAmount >= body?.total_amount) {
+      console.log("El pago se completó 😄");
+      try {
+        if (params.userId && params.userId.trim() !== "") {
+          const createUserResponse = await axios.post(`${DB_URL}/api/orders`, {
+            userId: params.userId,
+            paymentId: requestId,
+            status: body.order_status,
+            total: body.total_amount,
+            products: body.items,
+            preferenceId: body.preference_id,
+          });
+          if (createUserResponse.status === 200) {
+            console.log("Order Created");
+          }
+        }
+      } catch (error) {
+        console.log("Error al crear la orden:", error.message);
+      }
+    } else {
+      console.log("El pago NO se completó 😔");
+    }
+
+    res.send();
   } catch (error) {
-    console.error("Error:", error);
-    res.sendStatus(500);
+    console.log("Error en receiveWebHook:", error.message);
+    res.status(500).json({ error: error.message });
   }
 };
 
