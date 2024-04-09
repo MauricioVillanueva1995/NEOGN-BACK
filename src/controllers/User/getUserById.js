d9c9181b2505aab2ba4ff0209f4ae7b7f84c36b8
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-04-09 17:00:40 -0300

    fixes

--- src/controllers/User/getUserById.js
+++ src/controllers/User/getUserById.js
@@ -1,8 +1,8 @@
 const db = require("../../db");
 
-const getUser = async (clientId) => {
+const getUser = async (id) => {
   try {
-    const user = await db.User.findOne({ where: { clientId } });
+    const user = await db.User.findOne({ where: { id } });
     if (!user) {
       throw new Error("User not found");
     }
