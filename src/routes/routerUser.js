0d84895fac67f38a23a46bc1b5ef67b9f95152b3
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2024-04-09 23:52:30 -0300

    fixes

--- src/routes/routerUser.js
+++ src/routes/routerUser.js
@@ -35,6 +35,18 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// Log In
+router.get("/logIn/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
 // Registrarse
 router.post("/signUp", async (req, res) => {
   try {
